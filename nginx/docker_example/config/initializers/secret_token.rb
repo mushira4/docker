ca67b02c66164653a313dd2c04b167ce96993ccc
ekashivagui <mushira4@gmail.com>
2016-01-18 03:06:36 -0200

    First commit

---
+++ nginx/docker_example/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DockerExample::Application.config.secret_key_base = '69aab5960e059b372048ff498aacc3b423dd9c503c5fd89fce486d1f0aa3fbf8fd1305adc856ccfde7f204f75447ee3c41f7e3ce36e58451ada46d74dfe41cce'
---
+++ nginx/docker_example/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DockerExample::Application.config.session_store :cookie_store, key: '_docker_example_session'
